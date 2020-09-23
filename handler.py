51459fe3dee10f7486282f3ef5562cdc7d1dc38c
Sage Rimal <srimal@campusexplorer.com>
2020-09-22 18:18:32 -0700

    Update handler.py

--- handler.py
+++ handler.py
@@ -12,65 +12,96 @@ from boto3.dynamodb.conditions import Key
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-sns = boto3.client('sns')
+
 dynamodb = boto3.resource('dynamodb')
 dynamo = boto3.client('dynamodb')
+sns = boto3.client(
+    "sns",
+    region_name="us-west-2"
+)
 
 def lambda_handler(event, context):
     '''convert current time into PST '''
-    now = datetime.datetime.now() - timedelta(hours=8)
+    now = datetime.datetime.now() - timedelta(hours=7)
     time_= now.strftime("%Y-%m-%d%H")
-    site_url = {YOUR_S3_SITE_URL}
+    time_now = now.strftime("%Y-%m-%d%H")
+    #site_url = {YOUR_S3_SITE_URL}
 
     """
     Check to see the event type: either user generated, or cloudwatch event.
     Only use dynamoDB query if the event source is cloudwatch, and not UI
     Easy way to find out the event type is in the event headers. 
     """
-    
     try:
         site_url = event["headers"]["referer"]
     except:
         site_url = None 
-    
+    print(event)
     if site_url:
         name = str(event["queryStringParameters"]['firstname'])
         message = str(event["queryStringParameters"]['message'])
         phone = str(event["queryStringParameters"]['phone'])
         date = str(event["queryStringParameters"]['date'])
-        id = str(uuid.uuid1())
         date_ = date.replace('T', ' ')
         pattern = '%Y-%m-%d %H:%M'
         epoch_ = ''.join(date_.split())[:-3]
+        id = str(uuid.uuid1())
+        
+        
+        def get_dates(date_):
+
+            time_ = date_
+            hour =  time_[-2:]
+        
+            for x in range(1,5):
+                
+                date_time_obj = datetime.datetime.strptime(time_, '%Y-%m-%d%H%f')
+                new_date = date_time_obj+ datetime.timedelta(days=1)
+            
+                next_date = new_date.strftime("%Y-%m-%d")
+        
+                next_date_time = next_date + hour 
+                time_ = str(next_date_time)
+                id = str(uuid.uuid1())
+                dynamo.put_item(TableName='iots',Item={'id':{'S':id}, 'time':{'S':time_},'repeat': {'S': repeat}, 'firstname': {'S': name}, 'usr_time':{'S':time_}, 'message':{'S':message}, 'phone':{'S':phone}})
+
+        
+        try:
+            repeat = str(event["queryStringParameters"]['repeat'])
+        except:
+            repeat = "none"
+   
 
         """ Add item to the table if the source is not cloudwatch event """
-        dynamo.put_item(TableName='iots',
-                        Item={'id':{'S':id},
-                        'time':{'S':epoch_},
-                        'usr_time':{'S':time_}, 
-                        'message':{'S':message}, 
-                        'phone':{'S':phone}})
-    
+        if repeat != 'reccuring':
+            dynamo.put_item(TableName='iots',Item={'id':{'S':id}, 'time':{'S':epoch_},'repeat': {'S': repeat}, 'firstname': {'S': name}, 'usr_time':{'S':time_}, 'message':{'S':message}, 'phone':{'S':phone}})
+        else:
+            get_dates(epoch_)
+            
         return { 
             'statusCode': 200, 
             'body': 'Thank you for the response' 
         }
-    """ if the source doesn't have the right header, then just check the table to see if the record exists """
+     
     else:
-    """ Send a SNS message if the record exists """
-       table = dynamodb.Table('iots')
-       response = table.get_item(Key={'time': time_ })
-       try:
-           item = response['Item']
-           phone = response['Item']['phone']
-           message = response['Item']['message']
-       except (KeyError, TypeError) as error:
-           print(error)
-           item = None
+         
+      try:
+          table = dynamodb.Table('iots')
+          response = table.get_item(Key={'time': epoch_ })
+          print(f'MY RESPONSE IS {response}')
+          item = response['Item']
+          phone = response['Item']['phone']
+          message = response['Item']['message']
+          table.delete_item(Key={'time': time_now })
+      except (KeyError, TypeError) as error:
+          print(error)
+          item = None
 
-       if item != None:
-           print('this will also show up in cloud watch')
-           logger.info('got event{}'.format(event))
-           sns.publish(PhoneNumber=phone, Message=message)
-       else:
-           print("Something went wrong")
+      if item != None:
+          print('this will also show up in cloud watch')
+     
+          sns.publish(PhoneNumber=phone, Message=message)
+            
+      else:
+          print("Something went wrong")
+  
